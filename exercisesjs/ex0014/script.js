809bd273de8e75ff5d527d1fcf7ee56e8e2390ee
Paulo <98671374+Paulogameprays@users.noreply.github.com>
2023-07-25 17:31:24 -0300

    Função load pra verificar a hora

--- exercisesjs/ex0014/script.js
+++ exercisesjs/ex0014/script.js
@@ -1,1 +1,25 @@
-alert("Hello")
+
+function load() 
+{
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var date = new Date()
+    var hour = date.getHours()
+    msg.innerHTML = `Agora são ${hour} horas.`
+    if (hour >= 0 && hour <= 12)
+    {
+        //Bom dia
+        img.src = 'imagens/fotomanha.png'
+        document.body.style.backgroundColor = '#fda97b'
+    } else if (hour > 12 && hour <= 18)
+    {
+        //Boa tarde
+        img.src = 'imagens/fototarde.png'
+        window.document.body.style.backgroundColor = '#f74a00'
+    } else
+    {
+        //Boa noite
+        img.src = 'imagens/fotonoite.png'
+        document.body.style.backgroundColor = '#463a60'
+    }
+}
